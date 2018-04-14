149c6d6fd6e357436bcac994031537c44ba082be
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2018-04-14 11:01:52 +0900

    neo init

---
+++ neocs/Escargot/Escargot/NeoSmartContract.cs
@@ -0,0 +1,10 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+
+public class NeoSmartContract : SmartContract
+{
+    public static bool Main()
+    {
+        return true;
+    }
+}
